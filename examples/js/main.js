fbb1c0c54e401713a0e60fb12867a10b9b12f0af
Pierre <pierre.besson7@gmail.com>
2014-02-20 00:35:01 +0100

    Add ajax working.

--- examples/js/main.js
+++ examples/js/main.js
@@ -2,7 +2,9 @@
 // Deal with the contene of the click on the navbar and display the page correctly.
 (function() {
 	var activeElement = document.location.hash.slice(1);
-	document.querySelector("a[data-ex='" + activeElement + "']").parentNode.classList.add('active');
+	var selector = "[data-ex='" + activeElement + "']";
+	document.querySelector("a" + selector).parentNode.classList.add('active');
+	document.querySelector("div" + selector).hidden = false;
 })();
 $('.navbar a[data-ex]').on('click', function(event) {
 	event.preventDefault();
@@ -20,7 +22,7 @@ $('.navbar a[data-ex]').on('click', function(event) {
 	document.querySelector(selector).hidden = false;
 
 	//Update the location url hash
-	document.location.hash = "#"+event.target.getAttribute('data-ex');
+	document.location.hash = "#" + event.target.getAttribute('data-ex');
 });
 
 
@@ -31,11 +33,57 @@ Demo = {};
 
 // ## AJAX
 (function(NS) {
-	var ajax = function(e) {
-		console.log(e);
+	var ajax = function(method, url) {
+		// Create the XHR object.
+		function createCORSRequest(method, url) {
+			var xhr = new XMLHttpRequest();
+			if ("withCredentials" in xhr) {
+				// XHR for Chrome/Firefox/Opera/Safari.
+				xhr.open(method, url, true);
+			} else if (typeof XDomainRequest != "undefined") {
+				// XDomainRequest for IE.
+				xhr = new XDomainRequest();
+				xhr.open(method, url);
+			} else {
+				// CORS not supported.
+				xhr = null;
+			}
+			return xhr;
+		}
+
+		// Helper method to parse the title tag from the response.
+		function getTitle(text) {
+			return text.match('<title>(.*)?</title>')[1];
+		}
+
+		// Make the actual CORS request.
+		function makeCorsRequest(method, url) {
+			// All HTML5 Rocks properties support CORS.
+			var xhr = createCORSRequest(method, url);
+			if (!xhr) {
+				alert('CORS not supported');
+				return;
+			}
+
+			// Response handlers.
+			xhr.onload = function() {
+				var text = xhr.responseText;
+				var title = getTitle(text);
+				$('div[data-ex="ajax"] .container').html(title);
+			};
+
+			xhr.onerror = function() {
+				$('div[data-ex="ajax"] .container').html('Woops, there was an error making the request.');
+			};
+
+			xhr.send();
+		}
+		makeCorsRequest(method, url);
 	};
 	NS.ajax = ajax;
 	return ajax;
 })(Demo);
-
-Demo.ajax('Papa', 'Singe');
+//Register the button ajax on the demo page.
+$('button#ajax').on('click', function(event) {
+	Demo.ajax('GET', 'http://updates.html5rocks.com');
+});
--- slides/communication.md
+++ slides/communication.md
@@ -1,4 +1,19 @@
-# XMLHttpRequest
+# Communication
 --
-## GET
+## XMLHttpRequest
+```:javascript
+var req = new XMLHttpRequest();
+req.open('GET', 'http://www.mozilla.org/', true);
+req.onreadystatechange = function (aEvt) {
+  if (req.readyState == 4) {
+     if(req.status == 200)
+      dump(req.responseText);
+     else
+      dump("Erreur pendant le chargement de la page.\n");
+  }
+};
+req.send(null);	
+```
+- [MDN](https://developer.mozilla.org/fr/docs/XMLHttpRequest)
+- [Example](../examples/index.html#ajax)
 
